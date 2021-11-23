d5994c0074e25f8e931a349d860b212029bdc675
Flutter_SakuSaku13:44 <gitonly543@gmail.com>
2021-11-23 22:34:53 +0900

    コントローラーの紐付けできた

---
+++ MyColorMemoApp/HomeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HomeViewController.swift
+//  MyColorMemoApp
+//
+//  Created by 橋本純一 on 2021/11/23.
+//
+
+import Foundation
+import UIKit
+
+class HomeViewControllere: UIViewController {
+    override func viewDidLoad() {
+        print("HomeViewControllereが表示されました!")
+    }
+}
